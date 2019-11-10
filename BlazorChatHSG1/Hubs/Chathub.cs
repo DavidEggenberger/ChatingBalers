d283441bccf2b5fc2163bd5049c0c82ac141837a
DannyAinge <48399272+DannyAinge@users.noreply.github.com>
2019-11-10 21:24:14 +0100

    Add files via upload

---
+++ BlazorChatHSG1/Hubs/Chathub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChatHSG1.Hubs
+{
+    public class Chathub : Hub
+    {
+        public async Task SendMessage(string nachricht)
+        {
+            await Clients.All.SendAsync("Empfangen", nachricht);
+        }
+    }
+}
