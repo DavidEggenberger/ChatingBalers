90490896cb2763857be951b2ffb91fbcf3168569
DannyAinge <48399272+DannyAinge@users.noreply.github.com>
2019-11-17 15:39:48 +0100

    Add files via upload

---
+++ DataTypes/Nachricht.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChat.DataTypes
+{
+    public class Nachricht
+    {
+        public string Message { get; set; }
+        public string MessageFormatiert => " " + Message;
+        public DateTime GeschriebenZeit { get; set; }
+        public Nutzer Verfasser { get; set; }
+        public Nachricht(Nutzer n, string s = "")
+        {
+            GeschriebenZeit = DateTime.UtcNow;
+            Message = s;
+            Verfasser = n;
+        }
+    }
+}
